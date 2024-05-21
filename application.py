eff759c44be735d678473c89acf2709b3982777f
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-22 11:33:36 +1200

    Made a function for printing all motorcycles.

--- application.py
+++ application.py
@@ -1,9 +1,19 @@
+#Docstring - Darren Leung - Motorcycle Database Application
+#Imports
 import sqlite3
 
-db = sqlite3.connect("motorcycles.db")
-cursor = db.cursor()
-sql = "SELECT * FROM motorcycle;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close
+#Constants and Variables
+DATABASE = "motorcycles.db"
+
+#Functions
+def print_all_motorcycle():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM motorcycle;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(results)
+    db.close
+
+#Main Code
+print_all_motorcycle()
