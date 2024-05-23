65e8b69b686584c91779436119bd7f2a6556ca4a
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-24 10:29:53 +1200

    Made printing the data more prettier.

--- application.py
+++ application.py
@@ -12,7 +12,11 @@ def print_all_motorcycle():
     sql = "SELECT * FROM motorcycle;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print(results)
+    #loop through all the results
+    print("Model              ", "Year          ", "Engine_Size   ", "Horsepower    ", "Top_Speed     ", "Weight        ", "Price         ", "Description")
+    for motorcycle in results:
+        print(f"{motorcycle[1]:<20}{motorcycle[2]:<15}{motorcycle[3]:<15}{motorcycle[4]:<15}{motorcycle[5]:<15}{motorcycle[6]:<15}{motorcycle[7]:<15}{motorcycle[8]}")
+    #loop finished
     db.close
 
 #Main Code
