a405d1692b7b521a7ef0601e1ab74aec801bb8d2
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-24 10:50:53 +1200

    Made a menu for printing all motorcycles.

--- application.py
+++ application.py
@@ -6,18 +6,25 @@ import sqlite3
 DATABASE = "motorcycles.db"
 
 #Functions
-def print_all_motorcycle():
+def print_all_motorcycles():
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     sql = "SELECT * FROM motorcycle;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    #loop through all the results
+    #Loop through all the results
     print("Model              ", "Year          ", "Engine_Size   ", "Horsepower    ", "Top_Speed     ", "Weight        ", "Price         ", "Description")
     for motorcycle in results:
         print(f"{motorcycle[1]:<20}{motorcycle[2]:<15}{motorcycle[3]:<15}{motorcycle[4]:<15}{motorcycle[5]:<15}{motorcycle[6]:<15}{motorcycle[7]:<15}{motorcycle[8]}")
-    #loop finished
+    #Loop finished
     db.close
 
 #Main Code
-print_all_motorcycle()
+while True:
+    user_input = input("\nWhat would you like to do?\n1. Print all motorcycles\n2. Exit\n")
+    if user_input == "1":
+        print_all_motorcycles()
+    elif user_input == "2":
+        break
+    else:
+        print("That was not an option.\n")
