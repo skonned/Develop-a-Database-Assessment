59b5ed33adabd0712a35da9e6daf8ef8de637b40
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-28 11:49:40 +1200

    Added an option to see motorcycles from cheapest to most expensive.

--- application.py
+++ application.py
@@ -2,8 +2,10 @@
 #Imports
 import sqlite3
 
+
 #Constants and Variables
 DATABASE = "motorcycles.db"
+
 
 #Functions
 def print_all_motorcycles():
@@ -19,12 +21,29 @@ def print_all_motorcycles():
     #Loop finished
     db.close
 
+
+def motorcycles_from_cheapest_to_most_expensive():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT model, price FROM motorcycle ORDER BY price ASC"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #Loop through all the results
+    print("Model              ", "Price         ")
+    for motorcycle in results:
+        print(f"{motorcycle[0]:<20}{motorcycle[1]}")
+    #Loop finished
+    db.close
+
+
 #Main Code
 while True:
-    user_input = input("\nWhat would you like to do?\n1. Print all motorcycles\n2. Exit\n")
+    user_input = input("\nWhat would you like to do?\n1. Print all motorcycles\n2. See motorcycles from cheapest to most expensive\n3. Exit\n")
     if user_input == "1":
         print_all_motorcycles()
     elif user_input == "2":
+        motorcycles_from_cheapest_to_most_expensive()
+    elif user_input == "3":
         break
     else:
         print("That was not an option.\n")
