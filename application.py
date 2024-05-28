a683508f67505520ed4a57193e717362fa197cd7
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-29 10:36:24 +1200

    Added an option to see motorcycle manufacturers in the menu.

--- application.py
+++ application.py
@@ -25,7 +25,7 @@ def print_all_motorcycles():
 def motorcycles_from_cheapest_to_most_expensive():
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
-    sql = "SELECT model, price FROM motorcycle ORDER BY price ASC"
+    sql = "SELECT model, price FROM motorcycle ORDER BY price ASC;"
     cursor.execute(sql)
     results = cursor.fetchall()
     #Loop through all the results
@@ -36,14 +36,30 @@ def motorcycles_from_cheapest_to_most_expensive():
     db.close
 
 
+def see_motorcycle_manufacturers():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT motorcycle.model, manufacturer.name FROM motorcycle JOIN manufacturer ON manufacturer.id=motorcycle.manufacturer_id;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #Loop through all the results
+    print("Model              ", "Manufacturer         ")
+    for motorcycle in results:
+        print(f"{motorcycle[0]:<20}{motorcycle[1]}")
+    #Loop finished
+    db.close
+
+
 #Main Code
 while True:
-    user_input = input("\nWhat would you like to do?\n1. Print all motorcycles\n2. See motorcycles from cheapest to most expensive\n3. Exit\n")
+    user_input = input("\nWhat would you like to do?\n1. Print all motorcycles\n2. See motorcycles from cheapest to most expensive\n3. See motorcycle manufacturers\n4. Exit\n")
     if user_input == "1":
         print_all_motorcycles()
     elif user_input == "2":
         motorcycles_from_cheapest_to_most_expensive()
     elif user_input == "3":
+        see_motorcycle_manufacturers()
+    elif user_input == "4":
         break
     else:
         print("That was not an option.\n")
