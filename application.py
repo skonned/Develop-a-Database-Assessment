c37884575bcf1ab5213ccb865d5893a0eb1db482
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-04 10:51:24 +1200

    Completed the option where you can see information on a specific motorcycle by adding options for each individual motorcycle.

--- application.py
+++ application.py
@@ -71,7 +71,133 @@ def see_YZF_R6():
     cursor.execute(sql)
     results = cursor.fetchall()
     #Loop through all the results
-    print("Model              ", "Year          ", "Engine_Size   ", "Horsepower    ", "Top_Speed     ", "Weight        ", "Price         ", "Description")
+    print("\nModel              ", "Year          ", "Engine_Size   ", "Horsepower    ", "Top_Speed     ", "Weight        ", "Price         ", "Description")
+    for motorcycle in results:
+        print(f"{motorcycle[1]:<20}{motorcycle[2]:<15}{motorcycle[3]:<15}{motorcycle[4]:<15}{motorcycle[5]:<15}{motorcycle[6]:<15}{motorcycle[7]:<15}{motorcycle[8]}")
+    #Loop finished
+    db.close
+
+
+def see_CBR1000RR():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM motorcycle WHERE model = 'CBR1000RR'"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #Loop through all the results
+    print("\nModel              ", "Year          ", "Engine_Size   ", "Horsepower    ", "Top_Speed     ", "Weight        ", "Price         ", "Description")
+    for motorcycle in results:
+        print(f"{motorcycle[1]:<20}{motorcycle[2]:<15}{motorcycle[3]:<15}{motorcycle[4]:<15}{motorcycle[5]:<15}{motorcycle[6]:<15}{motorcycle[7]:<15}{motorcycle[8]}")
+    #Loop finished
+    db.close
+
+
+def see_Ninja_400():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM motorcycle WHERE model = 'Ninja 400'"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #Loop through all the results
+    print("\nModel              ", "Year          ", "Engine_Size   ", "Horsepower    ", "Top_Speed     ", "Weight        ", "Price         ", "Description")
+    for motorcycle in results:
+        print(f"{motorcycle[1]:<20}{motorcycle[2]:<15}{motorcycle[3]:<15}{motorcycle[4]:<15}{motorcycle[5]:<15}{motorcycle[6]:<15}{motorcycle[7]:<15}{motorcycle[8]}")
+    #Loop finished
+    db.close
+
+
+def see_Panigale_V4():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM motorcycle WHERE model = 'Panigale V4'"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #Loop through all the results
+    print("\nModel              ", "Year          ", "Engine_Size   ", "Horsepower    ", "Top_Speed     ", "Weight        ", "Price         ", "Description")
+    for motorcycle in results:
+        print(f"{motorcycle[1]:<20}{motorcycle[2]:<15}{motorcycle[3]:<15}{motorcycle[4]:<15}{motorcycle[5]:<15}{motorcycle[6]:<15}{motorcycle[7]:<15}{motorcycle[8]}")
+    #Loop finished
+    db.close
+
+
+def see_Street_Glide():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM motorcycle WHERE model = 'Street Glide'"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #Loop through all the results
+    print("\nModel              ", "Year          ", "Engine_Size   ", "Horsepower    ", "Top_Speed     ", "Weight        ", "Price         ", "Description")
+    for motorcycle in results:
+        print(f"{motorcycle[1]:<20}{motorcycle[2]:<15}{motorcycle[3]:<15}{motorcycle[4]:<15}{motorcycle[5]:<15}{motorcycle[6]:<15}{motorcycle[7]:<15}{motorcycle[8]}")
+    #Loop finished
+    db.close
+
+
+def see_GSX_R750():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM motorcycle WHERE model = 'GSX-R750'"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #Loop through all the results
+    print("\nModel              ", "Year          ", "Engine_Size   ", "Horsepower    ", "Top_Speed     ", "Weight        ", "Price         ", "Description")
+    for motorcycle in results:
+        print(f"{motorcycle[1]:<20}{motorcycle[2]:<15}{motorcycle[3]:<15}{motorcycle[4]:<15}{motorcycle[5]:<15}{motorcycle[6]:<15}{motorcycle[7]:<15}{motorcycle[8]}")
+    #Loop finished
+    db.close
+
+
+def see_S1000RR():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM motorcycle WHERE model = 'S1000RR'"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #Loop through all the results
+    print("\nModel              ", "Year          ", "Engine_Size   ", "Horsepower    ", "Top_Speed     ", "Weight        ", "Price         ", "Description")
+    for motorcycle in results:
+        print(f"{motorcycle[1]:<20}{motorcycle[2]:<15}{motorcycle[3]:<15}{motorcycle[4]:<15}{motorcycle[5]:<15}{motorcycle[6]:<15}{motorcycle[7]:<15}{motorcycle[8]}")
+    #Loop finished
+    db.close
+
+
+def see_Street_Triple():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM motorcycle WHERE model = 'Street Triple'"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #Loop through all the results
+    print("\nModel              ", "Year          ", "Engine_Size   ", "Horsepower    ", "Top_Speed     ", "Weight        ", "Price         ", "Description")
+    for motorcycle in results:
+        print(f"{motorcycle[1]:<20}{motorcycle[2]:<15}{motorcycle[3]:<15}{motorcycle[4]:<15}{motorcycle[5]:<15}{motorcycle[6]:<15}{motorcycle[7]:<15}{motorcycle[8]}")
+    #Loop finished
+    db.close
+
+
+def see_Duke_390():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM motorcycle WHERE model = 'Duke 390'"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #Loop through all the results
+    print("\nModel              ", "Year          ", "Engine_Size   ", "Horsepower    ", "Top_Speed     ", "Weight        ", "Price         ", "Description")
+    for motorcycle in results:
+        print(f"{motorcycle[1]:<20}{motorcycle[2]:<15}{motorcycle[3]:<15}{motorcycle[4]:<15}{motorcycle[5]:<15}{motorcycle[6]:<15}{motorcycle[7]:<15}{motorcycle[8]}")
+    #Loop finished
+    db.close
+
+
+def see_RSV4():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM motorcycle WHERE model = 'RSV4'"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #Loop through all the results
+    print("\nModel              ", "Year          ", "Engine_Size   ", "Horsepower    ", "Top_Speed     ", "Weight        ", "Price         ", "Description")
     for motorcycle in results:
         print(f"{motorcycle[1]:<20}{motorcycle[2]:<15}{motorcycle[3]:<15}{motorcycle[4]:<15}{motorcycle[5]:<15}{motorcycle[6]:<15}{motorcycle[7]:<15}{motorcycle[8]}")
     #Loop finished
@@ -91,10 +217,28 @@ while True:
         see_motorcycle_descriptions()
     elif user_input == "5":
         while True:
-            user_input2 = input("\nWhich motorcycle would you like to see?\n1. YZF-R6\n2. Exit\n")
+            user_input2 = input("\n\nWhich motorcycle would you like to see?\n1. YZF-R6\n2. CBR1000RR\n3. Ninja 400\n4. Panigale V4\n5. Street Glide\n6. GSX-R750\n7. S1000RR\n8. Street Triple\n9. Duke 390\n10. RSV4\n11. Return to Main Menu\n")
             if user_input2 == "1":
                 see_YZF_R6()
             elif user_input2 == "2":
+                see_CBR1000RR()
+            elif user_input2 == "3":
+                see_Ninja_400()
+            elif user_input2 == "4":
+                see_Panigale_V4()
+            elif user_input2 == "5":
+                see_Street_Glide()
+            elif user_input2 == "6":
+                see_GSX_R750()
+            elif user_input2 == "7":
+                see_S1000RR()
+            elif user_input2 == "8":
+                see_Street_Triple()
+            elif user_input2 == "9":
+                see_Duke_390()
+            elif user_input2 == "10":
+                see_RSV4()
+            elif user_input2 == "11":
                 break
             else:
                 print("That was not an option.\n")
